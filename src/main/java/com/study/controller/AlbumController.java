082a1f2d24c7894fe677b6872dc7add5403e5445
1822718083@qq.com <1822718083@qq.com>
2019-03-10 23:41:57 +0800

    简单替换UI

--- src/main/java/com/study/controller/AlbumController.java
+++ src/main/java/com/study/controller/AlbumController.java
@@ -1,14 +1,21 @@
 package com.study.controller;
 
+import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.study.model.Album;
+import com.study.model.User;
 import com.study.service.AlbumService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.awt.print.Pageable;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
@@ -21,12 +28,12 @@ public class AlbumController {
     public Map<String,Object> getAll(Album album, String draw,
                                      @RequestParam(required = false, defaultValue = "1") int start,
                                      @RequestParam(required = false, defaultValue = "10") int length){
-        Map<String,Object> map = new HashMap<>();
+        JSONObject jsonObject = new JSONObject();
         PageInfo<Album> pageInfo = albumService.selectByPage(album, start, length);
-        map.put("draw",draw);
-        map.put("recordsTotal",pageInfo.getTotal());
-        map.put("recordsFiltered",pageInfo.getTotal());
-        map.put("data", pageInfo.getList());
-        return map;
+        jsonObject.put("count", pageInfo.getTotal());
+        jsonObject.put("data", pageInfo.getList());
+        jsonObject.put("code", 0);
+        jsonObject.put("msg", "success");
+        return jsonObject;
     }
 }
